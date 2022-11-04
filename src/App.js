0adf26b4087941dfdfa46ef198a2c9033379f195
rlukashenko <geroinya92@gmail.com>
2022-11-04 09:56:00 +0700

    sticky_container1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './components/header'
+import './App.css';
+import Main from './components/main';
+
+function App() {
+  return (
+    <body className='body'>
+      
+        <header className="App-header">
+          <Header />
+        </header>
+       
+      
+    </body>
+
+  );
+}
+
+export default App;
+/*<main className='Main'>
+<Main />
+</main>*/
